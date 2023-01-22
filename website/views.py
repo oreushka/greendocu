8774a4a7c929ed0e845537dcebcae63c54fc6bf6
Besenok-13 <artbondar2003@gmail.com>
2023-01-23 00:20:07 +0300

    ready_for_play_and_pray

--- website/views.py
+++ website/views.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 13 21:16:55 2021
+Created on Wed Oct 13 21:16:55 2022
 
 @author: artbo
 """
 
-from flask import Blueprint, flash, redirect, render_template, request, url_for, jsonify
-from flask_login import current_user, login_required
 import json
+
+from flask import Blueprint, flash, jsonify, redirect, render_template, request, url_for
+from flask_login import current_user, login_required
 from . import db
-from .models import *
+from .auth import *
 from .greendocu_gen import make_matrix
+from .models import *
 
 views = Blueprint("views", __name__)
 
@@ -27,15 +29,39 @@ def game():
     return render_template("game.html", user=current_user, matrix=json.dumps(matrix))
 
 
-@views.route("/profile")
+@views.route("/profile", methods=["GET", "POST"])
+@login_required
 def profile():
     users = User.query.order_by(User.id)
-    return render_template("profile.html", users=users, user=current_user)
+    win = request.args.get('win')
+    if win:
+        return render_template("profile.html", users=users, user=current_user, win=True)
+        
+    return render_template("profile.html", users=users, user=current_user, win=False)
+
 
 @views.route("/make_matrix")
 def create():
-    matrix = make_matrix(to_del=20)
+    matrix = make_matrix(to_del=2)
     return jsonify(matrix)
+
+
+@views.route("/increase_victories", methods=["GET", "POST"])
+def increase_victories():
+    if current_user.is_anonymous:
+        return redirect(url_for("views.home", user=None))
+
+    update_user_statistic(victories_flag=True)
+    return redirect(url_for("views.profile", user=current_user, win=True))
+
+
+@views.route("/started_play", methods=["GET", "POST"])
+def started_play():
+    if current_user.is_anonymous:
+        return redirect(url_for("views.home", user=None))
+        
+    update_user_statistic(games_flag=True)
+    return redirect(url_for("views.home", user=current_user))
 
 
 """
