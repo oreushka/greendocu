7e80fb16e353aa52a96a9d9c136415b33908aa15
sporrrtik <81933050+sporrrtik@users.noreply.github.com>
2023-01-23 08:33:20 +0300

    done

--- website/greendocu_gen_test.py
+++ website/greendocu_gen_test.py
@@ -13,7 +13,7 @@ def test_create_matrix():
                                               [5, 6, 7, 8, 9, 1, 2, 3, 4]]
 
 
-def test_delete_num(to_del = 80):
+def test_delete_num_1(to_del = 2):
     arr = [[1, 2, 3, 4, 5, 6, 7, 8, 9],
             [4, 5, 6, 7, 8, 9, 1, 2, 3],
             [7, 8, 9, 1, 2, 3, 4, 5, 6],
@@ -27,20 +27,69 @@ def test_delete_num(to_del = 80):
     assert sum([row.count(0) for row in arr]) == to_del
 
 
-def test_remake_for_js():
+def test_delete_num_3(to_del = 20):
+    arr = [[1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [4, 5, 6, 7, 8, 9, 1, 2, 3],
+            [7, 8, 9, 1, 2, 3, 4, 5, 6],
+            [9, 1, 2, 3, 4, 5, 6, 7, 8],
+            [3, 4, 5, 6, 7, 8, 9, 1, 2],
+            [6, 7, 8, 9, 1, 2, 3, 4, 5],
+            [8, 9, 1, 2, 3, 4, 5, 6, 7],
+            [2, 3, 4, 5, 6, 7, 8, 9, 1],
+            [5, 6, 7, 8, 9, 1, 2, 3, 4]]
+    arr = greendocu_gen.delite_num(arr, to_del)
+    assert sum([row.count(0) for row in arr]) == to_del
+
+
+
+def test_delete_num_4(to_del = 40):
+    arr = [[1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [4, 5, 6, 7, 8, 9, 1, 2, 3],
+            [7, 8, 9, 1, 2, 3, 4, 5, 6],
+            [9, 1, 2, 3, 4, 5, 6, 7, 8],
+            [3, 4, 5, 6, 7, 8, 9, 1, 2],
+            [6, 7, 8, 9, 1, 2, 3, 4, 5],
+            [8, 9, 1, 2, 3, 4, 5, 6, 7],
+            [2, 3, 4, 5, 6, 7, 8, 9, 1],
+            [5, 6, 7, 8, 9, 1, 2, 3, 4]]
+    arr = greendocu_gen.delite_num(arr, to_del)
+    assert sum([row.count(0) for row in arr]) == to_del
+
+
+def test_delete_num_5(to_del = 80):
+    arr = [[1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [4, 5, 6, 7, 8, 9, 1, 2, 3],
+            [7, 8, 9, 1, 2, 3, 4, 5, 6],
+            [9, 1, 2, 3, 4, 5, 6, 7, 8],
+            [3, 4, 5, 6, 7, 8, 9, 1, 2],
+            [6, 7, 8, 9, 1, 2, 3, 4, 5],
+            [8, 9, 1, 2, 3, 4, 5, 6, 7],
+            [2, 3, 4, 5, 6, 7, 8, 9, 1],
+            [5, 6, 7, 8, 9, 1, 2, 3, 4]]
+    arr = greendocu_gen.delite_num(arr, to_del)
+    assert sum([row.count(0) for row in arr]) == to_del
+
+
+def test_remake_for_js_1():
     arr = [[1,2],[0,4]]
     assert greendocu_gen.remake_for_js(arr)[0] == [[[1], [2]], [[], [4]]]
 
-# print(greendocu_gen.remake_for_js([[1,2],[0,4]]))
 
-# arr = greendocu_gen._create_matrix()
-# arr = greendocu_gen._shuffle_row(arr, 20)
-# # arr = greendocu_gen.delite_num(arr, 4)
-#
-# for i in range(9):
-#     for j in range(9):
-#         print(arr[i][j], end=' ')
-#     print('\n')
+def test_remake_for_js_2():
+    arr = [[0,0],[0,0]]
+    assert greendocu_gen.remake_for_js(arr)[0] == [[[], []], [[], []]]
+
+
+def test_remake_for_js_3():
+    arr = [[1,2],[3,4]]
+    assert greendocu_gen.remake_for_js(arr)[0] == [[[1], [2]], [[3], [4]]]
+
+def test_remake_for_js_at_all():
+    arr = [[1,0],[3,0]]
+    assert greendocu_gen.remake_for_js(arr) == ([[[1], []], [[3], []]], 2)
+
+
+print(greendocu_gen.remake_for_js([[1,0],[3,0]]))
 
 
 
